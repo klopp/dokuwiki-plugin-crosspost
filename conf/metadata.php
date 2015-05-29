9e06483c853e7d0bf88b2608ec93a7d7c9d87797
klopp <klopp@yandex.ru>
2015-05-29 09:57:17 +0300

    config fix

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,7 +1,7 @@
 <?php
 $meta['cp_ns_disabled'] = array('' 
 );
-$meta['cp_ns_disable_on'] = array('' 
+$meta['cp_disable_on'] = array('' 
 );
 $meta['cp_add_links'] = array('onoff' 
 );
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,6 +3,5 @@
 $lang['cp_ns_disabled'] = 'Disable crossposting to sections (&quot;!section&quot; - with subsections)';
 $lang['cp_disable_on'] = 'Do not show crosspost form in sections (&quot;!section&quot; - with subsections)';
 $lang['cp_add_links'] = 'Add links to pages with crossposts before article';
-$lang['cp_use_headers'] = 'Use page/section headers in links';
 $lang['cp_form_titles'] = 'Show section headers in page edit form';
 $lang['cp_link_titles'] = 'Show page headers in links';
