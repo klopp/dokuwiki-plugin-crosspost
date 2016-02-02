eab0a419e3d1fab772146c1e6d9ff632c170ad77
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:04:07 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -7,7 +7,7 @@ require_once (DOKU_PLUGIN . 'action.php');
 class action_plugin_crosspost extends DokuWiki_Action_Plugin
 {
 
-    function register(&$controller)
+    function register(Doku_Event_Handler $controller)
     {
         $controller->register_hook( 'ACTION_ACT_PREPROCESS', 'BEFORE', $this, 
                 'handle_act' );
